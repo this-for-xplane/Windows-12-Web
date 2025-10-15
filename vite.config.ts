e2913f4beeb61814f759b6360525bc4fcc7f4c4f
SeoJun <129959256+this-for-xplane@users.noreply.github.com>
2025-10-15 22:15:21 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Windows-12-Web/", // ✅ 반드시 추가! (리포 이름과 동일하게)
   server: {
     host: "::",
     port: 8080,
@@ -16,3 +17,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
